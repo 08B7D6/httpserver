5a2c4950db9b77b7b92f19d26188b149fedcb674
08B7D6 <08b7d6@gmail.com>
2024-08-07 16:40:34 -0400

    Remove redundant error handling + close socket

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 
 int create_socket(){
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1){
 			perror("Unable to create socket");
-			exit(1);
+			exit(EXIT_FAILURE);
 			}
 	return sockfd;
 }
 
 int main(){
 	int sockfd = create_socket();
-	if (sockfd == -1){
-		return 1;
-	}
+	
 
-	printf("Socket created");
+	printf("Socket has been created");
+	
+	close(sockfd);
+
 	return 0;
 }

9ccadb24e4058752f73a7dd63c323cadf9b441a5
08B7D6 <08b7d6@gmail.com>
2024-08-07 12:58:53 -0400

    Create Sockets

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <sys/socket.h>
+
+int create_socket(){
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1){
+			perror("webserver (socket)");
+			}
+	return sockfd;
+}
 
 int main(){
-	printf("Hello World");
+	int sockfd = create_socket();
+	if (sockfd == -1){
+		return 1;
+	}
+
+	printf("Socket Created");
 	return 0;
 }
